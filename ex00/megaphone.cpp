02c224e33184b4e46dd981d90ed2cb36e55f3754
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-03-07 18:56:40 +0900

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/07 18:43:15 by myokono           #+#    #+#             */
+/*   Updated: 2025/03/07 18:43:20 by myokono          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str = argv[i];
+		for (size_t j = 0; j < str.length(); j++)
+			std::cout << static_cast<char>(std::toupper(str[j]));
+	}
+	std::cout << std::endl;
+	return (0);
+}

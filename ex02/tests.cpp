3d8f9d4b4f540807ba19d0f0d045e322cfb74681
karen-kac <mai.cn@akane.waseda.jp>
2025-04-25 16:16:22 +0900

    ダウンロード

--- ex02/tests.cpp
+++ ex02/tests.cpp
@@ -1,25 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tests.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/07 18:52:24 by myokono           #+#    #+#             */
-/*   Updated: 2025/03/07 18:52:52 by myokono          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// ************************************************************************** //
+//                                                                            //
+//                tests.cpp for GlobalBanksters United                        //
+//                Created on  : Thu Nov 20 23:45:02 1989                      //
+//                Last update : Wed Jan 04 09:23:52 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
 
 #include <vector>
 #include <algorithm>
 #include <functional>
 #include "Account.hpp"
 
+
 int		main( void ) {
 
-	typedef std::vector<Account::t>							accounts_t;
-	typedef std::vector<int>								ints_t;
-	typedef std::pair<accounts_t::iterator, ints_t::iterator>	acc_int_t;
+	typedef std::vector<Account::t>							  accounts_t;
+	typedef std::vector<int>								  ints_t;
+	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
 
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
@@ -64,6 +62,7 @@ int		main( void ) {
 
 	return 0;
 }
+
 
 // ************************************************************************** //
 // vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //

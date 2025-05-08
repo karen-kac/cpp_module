a9e435c8ee74aad3e19d7fb0684fc20d5d559f27
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-05-08 16:04:12 +0900

    コピーコンストラクタとコピー代入演算子の整理

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,34 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.cpp                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/22 14:10:38 by myokono           #+#    #+#             */
-/*   Updated: 2025/03/22 14:11:51 by myokono          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Fixed.hpp"
 #include <iostream>
 
-Fixed::Fixed() : _fixedPointValue(0)
+Fixed::Fixed() : _rawBits(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& src)
+Fixed::Fixed(const Fixed& other)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	*this = src;
+	*this = other;
 }
 
-Fixed& Fixed::operator=(const Fixed& rhs)
+Fixed& Fixed::operator=(const Fixed& src)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &rhs) {
-		this->_fixedPointValue = rhs.getRawBits();
+	if (this != &src) {
+		this->_rawBits = src.getRawBits();
 	}
 	return *this;
 }
@@ -41,10 +29,10 @@ Fixed::~Fixed()
 int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return this->_fixedPointValue;
+	return this->_rawBits;
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	this->_fixedPointValue = raw;
+	this->_rawBits = raw;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,26 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/22 14:11:19 by myokono           #+#    #+#             */
-/*   Updated: 2025/03/22 14:11:27 by myokono          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 #include "Fixed.hpp"
 
 int main(void)
 {
 	Fixed a;
-	Fixed b(a);
+	Fixed b(a); // copy constructor
 	Fixed c;
 	
-	c = b;
-	
+	c = b; // copy assignment operator
+
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;

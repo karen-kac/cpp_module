8d8557ee93d59cb588438feb19c88f1a65f4e592
my-nozomi <mai.yokono@nozomi-ai.co.jp>
2025-05-08 14:57:03 +0900

    complainを変更して変数を整理しました

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,19 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/21 17:59:35 by myokono           #+#    #+#             */
-/*   Updated: 2025/03/21 17:59:47 by myokono          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Harl.hpp"
-
-const std::string Harl::levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-const Harl::t_func Harl::funcs[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
 Harl::Harl() {
 }
@@ -21,30 +6,44 @@ Harl::Harl() {
 Harl::~Harl() {
 }
 
-void Harl::debug(void) {
+void Harl::_debug(void) {
 	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
 	std::cout << "I really do!" << std::endl << std::endl;
 }
 
-void Harl::info(void) {
+void Harl::_info(void) {
 	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
 	std::cout << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl << std::endl;
 }
 
-void Harl::warning(void) {
+void Harl::_warning(void) {
 	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
 	std::cout << "I've been coming for years whereas you started working here since last month." << std::endl << std::endl;
 }
 
-void Harl::error(void) {
+void Harl::_error(void) {
 	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
 
 void Harl::complain(std::string level) {
+	void (Harl::*funcs[])() = {
+		&Harl::_debug,
+		&Harl::_info,
+		&Harl::_warning,
+		&Harl::_error
+	};
+	
+	std::string levels[] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+	
 	for (int i = 0; i < 4; i++) {
 		if (level == levels[i]) {
 			(this->*funcs[i])();
@@ -52,6 +51,5 @@ void Harl::complain(std::string level) {
 		}
 	}
 	
-	std::cout << "[ UNKNOWN LEVEL: " << level << " ]" << std::endl;
-	std::cout << "Harl doesn't know how to complain at this level." << std::endl << std::endl;
+	std::cout << "[ Invalid complaint level ]" << std::endl;
 }
--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Harl.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: myokono <myokono@student.42tokyo.jp>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/21 17:59:11 by myokono           #+#    #+#             */
-/*   Updated: 2025/03/21 17:59:27 by myokono          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef HARL_HPP
 #define HARL_HPP
 
@@ -19,15 +7,10 @@
 class Harl
 {
 private:
-	void debug(void);
-	void info(void);
-	void warning(void);
-	void error(void);
-
-	typedef void (Harl::*t_func)(void);
-
-	static const std::string levels[4];
-	static const t_func funcs[4];
+	void _debug(void);
+	void _info(void);
+	void _warning(void);
+	void _error(void);
 
 public:
 	Harl();

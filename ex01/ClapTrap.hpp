3b818180c58dedcb111207b31b6379d44652c829
karen-kac <mai.cn@akane.waseda.jp>
2025-05-09 15:01:17 +0900

    ex01でクラスの継承

---
+++ ex01/ClapTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap {
+protected:
+	std::string _name;
+	unsigned int _hitPoints;
+	unsigned int _energyPoints;
+	unsigned int _attackDamage;
+
+public:
+	// Orthodox Canonical Form
+	ClapTrap();
+	ClapTrap(const std::string& name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap();
+
+	// Member functions
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif

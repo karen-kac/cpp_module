e3119dd7c4bef041951342955cbd8d6481be474a
karen-kac <mai.cn@akane.waseda.jp>
2025-05-12 14:36:25 +0900

    ex01

---
+++ ex01/Brain.cpp
@@ -0,0 +1,29 @@
+#include "Brain.hpp"
+
+Brain::Brain() {
+	for (int i = 0; i < 100; i++) {
+		this->_ideas[i] = "";
+	}
+	std::cout << "Brain default constructor called" << std::endl;
+}
+
+Brain::Brain(const Brain& other) {
+	for (int i = 0; i < 100; i++) {
+		this->_ideas[i] = other._ideas[i];
+	}
+	std::cout << "Brain copy constructor called" << std::endl;
+}
+
+Brain& Brain::operator=(const Brain& other) {
+	if (this != &other) {
+		for (int i = 0; i < 100; i++) {
+			this->_ideas[i] = other._ideas[i];
+		}
+	}
+	std::cout << "Brain assignment operator called" << std::endl;
+	return *this;
+}
+
+Brain::~Brain() {
+	std::cout << "Brain destructor called" << std::endl;
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,4 +1,40 @@
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
+#include "Brain.hpp"
 
+int main() {
+	std::cout << "=== Create Cat A ===" << std::endl;
+	Cat catA;
+
+	std::cout << "\n=== Copy-Construct Cat B from A ===" << std::endl;
+	Cat catB(catA);
+
+	std::cout << "\n=== Create Cat C and Assign A to C ===" << std::endl;
+	Cat catC;
+	catC = catA;
+
+	std::cout << "\n=== Create Dog X ===" << std::endl;
+	Dog dogX;
+
+	std::cout << "\n=== Copy-Construct Dog Y from X ===" << std::endl;
+	Dog dogY(dogX);
+
+	std::cout << "\n=== Create Dog Z and Assign X to Z ===" << std::endl;
+	Dog dogZ;
+	dogZ = dogX;
+
+	std::cout << "\n=== End of main (Destructors will be called) ===" << std::endl;
+	return 0;
+}
+
+
+// int main() {
+// 	const Animal* j = new Dog();
+// 	const Animal* i = new Cat();
+
+// 	delete j;
+// 	delete i;
+
+// 	return 0;
+// }

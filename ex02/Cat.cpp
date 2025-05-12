6c1c1f83a70856619d9506f6ea8f8a2e2d6f2f00
karen-kac <mai.cn@akane.waseda.jp>
2025-05-12 14:38:41 +0900

    setup for ex02

---
+++ ex02/Cat.cpp
@@ -0,0 +1,33 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal(){
+	this->type = "Cat";
+	this->_brain = new Brain();
+	std::cout << "Cat default constructor called" << std::endl;
+}
+
+Cat::Cat(const Cat& other) : Animal(other) {
+	this->_brain = new Brain(*other._brain);
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat& other) {
+	if (this != &other) {
+		type = other.type;
+		if (this->_brain)
+			delete this->_brain;
+		this->_brain = new Brain(*other._brain);
+	}
+	std::cout << "Cat assignment operator called" << std::endl;
+	return *this;
+}
+
+
+Cat::~Cat() {
+	delete this->_brain;
+	std::cout << "Cat destructor called" << std::endl;
+}
+
+void Cat::makeSound() const {
+	std::cout << "Meow" << std::endl;
+}

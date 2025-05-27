3d4116d23c7f4e67adb86eff9998d73ebd1fbe70
karen-kac <mai.cn@akane.waseda.jp>
2025-05-27 19:30:23 +0900

    setup for ex01

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,36 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+
+	public:
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw() { return "Grade too high"; }
+		};
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw() { return "Grade too low"; }
+		};
+
+		Bureaucrat();
+		Bureaucrat(const std::string &name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+		Bureaucrat &operator=(const Bureaucrat &other);
+		~Bureaucrat();
+
+		const std::string &getName() const;
+		int getGrade() const;
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat);
+
+#endif

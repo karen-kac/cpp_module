0fc1d9dab61e4dbdc5b68c40d895c9339fb713f5
my-nozomi <mai0627.my@gmail.com>
2025-09-21 20:20:14 +0900

    責任の分離

--- ex02/Bureaucrat.cpp
+++ ex02/Bureaucrat.cpp
@@ -54,18 +54,12 @@ void Bureaucrat::decrementGrade()
 
 void Bureaucrat::signForm(AForm &form)
 {
-	if (form.getIsSigned()) {
-		std::cout << this->_name << " couldn't sign " << form.getName() 
-				  << " because this form is already signed" << std::endl;
-		return;
-	}
 	try {
 		form.beSigned(*this);
-		std::cout << this->_name << " signed " << form.getName() << std::endl;
-	}
-	catch (std::exception &e) {
-		std::cout << this->_name << " couldn't sign " << form.getName() 
-				  << " because " << e.what() << std::endl;
+		std::cout << getName() << " signed " << form.getName() << "." << std::endl;
+	} catch (const std::exception &e) {
+		std::cout << getName() << " couldn't sign " << form.getName()
+				  << " because " << e.what() << "." << std::endl;
 	}
 }
 
@@ -73,13 +67,14 @@ void Bureaucrat::executeForm(AForm const &form) const
 {
 	try {
 		form.execute(*this);
-		std::cout << this->_name << " executed " << form.getName() << std::endl;
+		std::cout << getName() << " executed " << form.getName() << std::endl;
 	}
-	catch (std::exception &e) {
-		std::cout << this->_name << " couldn't execute " << form.getName() 
+	catch (const std::exception &e) {
+		std::cout << getName() << " couldn't execute " << form.getName() 
 				  << " because " << e.what() << std::endl;
 	}
 }
+
 std::ostream &operator<<(std::ostream &os, const Bureaucrat &bureaucrat)
 {
 	os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
